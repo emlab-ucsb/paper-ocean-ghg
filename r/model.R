a380db255750030ccee5adb06dd535fd7485d2ac
gmcdonald-sfg <gmcdonald@bren.ucsb.edu>
2025-02-12 14:59:45 -0800

    getting extrapolation/decomposition ready

---
+++ r/model.R
@@ -0,0 +1,145 @@
+annual_spatial_co2_emissions_ais_dark |>
+  dplyr::group_by(year) |>
+  dplyr::summarise(across(c(emissions_co2_mt,
+                            emissions_co2_dark_mt),
+                          ~sum(.,na.rm=TRUE))) |>
+  dplyr::ungroup()
+
+monthly_ais_to_dark_activity_extrapolation |>
+  dplyr::group_by(year = lubridate::year(month)) |>
+  dplyr::summarise(across(c(emissions_co2_mt,
+                            emissions_co2_mt_dark),
+                          ~sum(.,na.rm=TRUE))) |>
+  dplyr::ungroup()
+
+annual_extrapolation_dataset <- monthly_ais_to_dark_activity_extrapolation |>
+  dplyr::mutate(length_size_class_percentile = as.factor(length_size_class_percentile)) |>
+  dplyr::mutate(year = lubridate::year(month)) |>
+  dplyr::filter(year>2015)   |>
+  dplyr::group_by(year,
+                  fishing,
+                  length_size_class_percentile) |>
+  dplyr::summarise(emissions_co2_mt_total = sum(emissions_co2_mt) + sum(emissions_co2_mt_dark),
+                   kw_hours_total = sum(kw_hours) + sum(kw_hours_dark),
+                   distance_nm_total = sum(distance_nm) + sum(distance_nm_dark),
+                   hours_total = sum(hours) + sum(hours_dark)) |>
+  dplyr::ungroup() |>
+  dplyr::mutate(average_speed_knots = distance_nm_total/hours_total) |>
+  dplyr::select(-c(hours_total,distance_nm_total))
+# Using annual_extrapolation_dataset, use tidymodels to train a random forest model
+# to predict emissions
+set.seed(123)
+
+# Set initial split of data
+inital_split <- annual_extrapolation_dataset |>
+  rsample::initial_split(prop = 0.8)
+
+# Train random forest on initial split using parsnip and ranger
+model <- parsnip::rand_forest() |>
+  parsnip::set_engine("ranger", importance = "impurity") |>
+  parsnip::set_mode("regression") |>
+  parsnip::fit(emissions_co2_mt_total ~ ., data = rsample::training(inital_split))
+
+# Test performance of model on testing data
+predictions <- predict(model, rsample::testing(inital_split))$.pred |>
+  as.numeric()
+
+augmented_testing_dataset <- rsample::testing(inital_split) |>
+  dplyr::bind_cols(predictions = predictions)
+
+# Calculate performance metrics
+metrics <- yardstick::rsq_trad(data = augmented_testing_dataset,
+                           truth = emissions_co2_mt_total,
+                           estimate = predictions)
+
+augmented_testing_dataset |> 
+  ggplot(aes(x = emissions_co2_mt_total, y = predictions)) + 
+  geom_point()
+
+vip::vi(model) |>
+  ggplot(aes(x = Importance,
+             y = reorder(Variable,Importance))) +
+  geom_bar(stat = "identity") +
+  labs(x = "Feature importance",
+       y = "Model feature")
+
+# Find initial values of kw_hours and speed, by fishing and length_size_class_percentile, for decomposition
+starting_values <- annual_extrapolation_dataset |>
+  dplyr::filter(year == 2016) |>
+  dplyr::select(fishing,
+                length_size_class_percentile,
+                kw_hours_total_starting = kw_hours_total,
+                average_speed_knots_starting = average_speed_knots)
+
+predictions_dataset <- annual_extrapolation_dataset |>
+  left_join(starting_values, by  = c("fishing","length_size_class_percentile")) 
+
+final_prediction_dataset <- predictions_dataset |>
+  bind_cols(observed_kw_hours_observed_speed = predict(model, predictions_dataset)$.pred) |>
+  bind_cols(observed_kw_hours_constant_speed = predict(model, predictions_dataset  |>
+                                                                     dplyr::select(-average_speed_knots) |>
+                                                                     dplyr::rename(average_speed_knots = average_speed_knots_starting))$.pred)|>
+  bind_cols(constant_kw_hours_observed_speed = predict(model, predictions_dataset|>
+                                                                     dplyr::select(-kw_hours_total) |>
+                                                                     dplyr::rename(kw_hours_total = kw_hours_total_starting))$.pred)|>
+  bind_cols(constant_kw_hours_constant_speed = predict(model, predictions_dataset |>
+                                                         dplyr::select(-average_speed_knots) |>
+                                                         dplyr::rename(average_speed_knots = average_speed_knots_starting) |>
+                                                         dplyr::select(-kw_hours_total) |>
+                                                         dplyr::rename(kw_hours_total = kw_hours_total_starting))$.pred)
+
+
+
+theme_plot <- function(){
+  theme_minimal() %+replace%
+    theme(axis.title.x = element_text(face = "bold"),
+          axis.title.y = element_text(angle = 90,
+                                      face = "bold",
+                                      vjust = 3),
+          strip.text.x = element_text(angle = 0,
+                                      face = "bold"),
+          strip.text.y = element_text(angle = 0,
+                                      face = "bold"))}
+
+annual_extrapolation_dataset |>
+  tidyr::pivot_longer(-c(year,fishing,length_size_class_percentile)) |>
+  dplyr::mutate(fishing = ifelse(fishing,"Fishing","Non-fishing")) |>
+  dplyr::mutate(name = forcats::fct_relevel(name,
+                                            c("kw_hours_total"))) |>
+  ggplot(aes(x = year,y = value,color=length_size_class_percentile,group=length_size_class_percentile)) +
+  geom_line() +
+  facet_wrap(name~fishing,scales="free_y",ncol=2) + 
+  paletteer::scale_color_paletteer_d(palette = "jcolors::pal10",direction = -1) +
+  theme_plot()+
+  guides(color = guide_legend(reverse=TRUE)) +
+  scale_x_continuous(breaks = unique(final_prediction_dataset$year)) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(x = "",
+       y = "",
+       color = "Length percentile")+
+  theme(panel.grid.minor.x = element_blank())
+
+final_prediction_dataset |>
+  dplyr::group_by(year) |>
+  dplyr::summarise(across(c(observed_kw_hours_observed_speed,
+                            observed_kw_hours_constant_speed,
+                            constant_kw_hours_observed_speed,
+                            constant_kw_hours_constant_speed),
+                          ~sum(.,na.rm=TRUE))) |>
+  tidyr::pivot_longer(-year) |>
+  ggplot(aes(x = year, y = value, color = name)) +
+  geom_line() +
+  guides(color = guide_legend(reverse=TRUE)) +
+  scale_color_manual("Scenario",
+                     values = c("steelblue4",
+                                "steelblue2",
+                                "coral4",
+                                "coral2")) +
+  scale_x_continuous(breaks = unique(final_prediction_dataset$year)) +
+  scale_y_continuous(labels = scales::unit_format(unit = "M", scale = 1e-6)) +
+  labs(x = "",
+       y = "Predicted CO2 emissions (MT)") +
+  theme_plot() +
+  theme(panel.grid.minor.x = element_blank())
+
+                     
--- sql/monthly_ais_to_dark_activity_extrapolation.sql
+++ sql/monthly_ais_to_dark_activity_extrapolation.sql
@@ -39,7 +39,7 @@ WITH
     SUM(hours) hours,
     SUM(hours * main_engine_power_kw) kw_hours,
     SUM(emissions_co2_mt) emissions_co2_mt,
-    AVG(IF(hours = 0,0,distance_nm/hours)) average_speed_knots
+    SUM(distance_nm) distance_nm
   FROM
     `world-fishing-827.proj_ocean_ghg.daily_gridded_emissions_by_vessel_v20241121`
   JOIN
@@ -50,29 +50,58 @@ WITH
     vessel_engine_power_info
   USING
     (ssvid)
+  WHERE
+    EXTRACT(YEAR
+    FROM
+      date) <= 2024
   GROUP BY
     lon_bin,
     lat_bin,
     time,
     fishing,
-    length_size_class_percentile )
+    length_size_class_percentile ),
+  spatiotemporal_extrapolations AS(
+  SELECT
+    time month,
+    lon_bin,
+    lat_bin,
+    fishing,
+    length_size_class_percentile,
+    hours,
+    hours * ratio_dark_to_ais hours_dark,
+    kw_hours,
+    kw_hours * ratio_dark_to_ais kw_hours_dark,
+    emissions_co2_mt,
+    emissions_co2_mt * ratio_dark_to_ais emissions_co2_mt_dark,
+    distance_nm,
+    distance_nm * ratio_dark_to_ais distance_nm_dark
+  FROM
+    ais_activity_summary
+  LEFT JOIN
+    dark_to_ais_ratios
+  USING
+    (time,
+      lon_bin,
+      lat_bin,
+      fishing,
+      length_size_class_percentile))
 SELECT
-  time month,
+  month,
   fishing,
   length_size_class_percentile,
-  hours,
-  hours * ratio_dark_to_ais hours_dark,
-  kw_hours,
-  kw_hours * ratio_dark_to_ais kw_hours_dark,
-  emissions_co2_mt,
-  emissions_co2_mt * ratio_dark_to_ais emissions_co2_mt_dark,
-  average_speed_knots
+  SUM(hours) hours,
+  SUM(hours_dark) hours_dark,
+  SUM(kw_hours) kw_hours,
+  SUM(kw_hours_dark) kw_hours_dark,
+  SUM(emissions_co2_mt) emissions_co2_mt,
+  SUM(emissions_co2_mt_dark) emissions_co2_mt_dark,
+  SUM(distance_nm) distance_nm,
+  SUM(distance_nm_dark) distance_nm_dark,
+  SUM(distance_nm) / SUM(hours) average_speed_knots,
+  SUM(distance_nm_dark) / SUM(hours_dark) average_speed_knots_dark
 FROM
-  ais_activity_summary
-LEFT JOIN
-  dark_to_ais_ratios
-USING
-  (time,
-    fishing,
-    length_size_class_percentile)
-WHERE EXTRACT(YEAR FROM time) <= 2024
+  spatiotemporal_extrapolations
+GROUP BY
+  month,
+  fishing,
+  length_size_class_percentile
