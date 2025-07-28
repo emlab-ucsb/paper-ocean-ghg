c4a7f82b054e8b56264e45185092bbecce4828c2
gmcdonald-sfg <gmcdonald@bren.ucsb.edu>
2025-07-28 13:22:29 -0700

    resynging in batches 2

--- _targets_01_gfw_data_pull.R
+++ _targets_01_gfw_data_pull.R
@@ -26,7 +26,7 @@ list(
   # Define the version of the dark fleet dataset to pull
   tar_target(
     name = run_version_dark,
-    "v20250228"
+    "v20250701"
   ),
   # Set analysis start year
   tar_target(
