0d5adda6d2a21778cc697c145a5b00c51fbb6ea5
pcarbomestre <pcarbomestre@ucsb.edu>
2026-01-05 16:44:19 +0100

    2024 spatial emissions by fleet figure

---
+++ sql/annual_spatial_co2_emissions_ais_dark_by_fleet.sql
@@ -0,0 +1,16 @@
+SELECT
+    EXTRACT (YEAR FROM time) year,
+    lon_bin,
+    lat_bin,
+    fishing,
+    SUM(emissions_co2_mt) emissions_co2_mt,
+    SUM(emissions_co2_dark_mt) emissions_co2_dark_mt
+FROM
+  `world-fishing-827.proj_ocean_ghg.rf_s1_time_gridded_dark_fleet_model_{run_version_dark}`
+WHERE
+  EXTRACT(YEAR FROM time) BETWEEN {analysis_start_year} AND {analysis_end_year} 
+GROUP BY
+  year,
+  lon_bin,
+  lat_bin,
+  fishing
