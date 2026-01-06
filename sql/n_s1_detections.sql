faa85cda4dcb3002c48d347a307957291af86859
gmcdonald-sfg <gmcdonald@bren.ucsb.edu>
2026-01-06 16:01:50 -0500

    add unmatched s1 detections

--- sql/n_s1_detections.sql
+++ sql/n_s1_detections.sql
@@ -1,5 +1,6 @@
 SELECT
   COUNT(DISTINCT detect_id)/1e6 n_million_s1_detections,
+  COUNT(DISTINCT CASE WHEN detect_ssvid IS NULL  THEN detect_id END)/1e6 n_million_s1_detections_unmatched,
   COUNT(DISTINCT scene_id)/1e3 n_thousand_s1_scenes
 FROM
   `world-fishing-827.proj_ocean_ghg.rf_s1_detections_with_vessel_info_{run_version_dark}`
