1724f608280d89aab6fcdc1be3f7eb4439af862c
gmcdonald-sfg <gmcdonald@bren.ucsb.edu>
2026-01-20 14:25:00 -0800

    new figs

--- sql/annual_global_emissions_by_receiver_type.sql
+++ sql/annual_global_emissions_by_receiver_type.sql
@@ -1,24 +1,38 @@
-SELECT
-  EXTRACT(YEAR FROM month) year,
+WITH
+all_data AS(
+  SELECT
+EXTRACT(YEAR FROM timestamp) year,
   receiver_type,
+  ssvid,
+  emissions_co2_mt
+FROM
+  `world-fishing-827.proj_ocean_ghg.ping_level_emissions_{run_version_ais}`
+  WHERE
+    DATE(timestamp) BETWEEN '{analysis_start_year}-01-01'
+    AND '{analysis_end_year}-12-31'
+)
+SELECT
+  year,
+  receiver_type,
+  COUNT(*) number_pings,
   SUM(emissions_co2_mt) emissions_co2_mt,
   COUNT(DISTINCT ssvid) n_distinct_ssvid
 FROM
-  `world-fishing-827.proj_ocean_ghg.monthly_spatial_vessel_emissions_by_receiver_type_{run_version_ais}`
-WHERE EXTRACT(YEAR FROM month) BETWEEN {analysis_start_year} AND {analysis_end_year}
+all_data
 GROUP BY
   year,
   receiver_type
-UNION ALL(
+  UNION ALL(
   SELECT
-  EXTRACT(YEAR FROM month) year,
+  year,
   'total' receiver_type,
+  COUNT(*) number_pings,
   SUM(emissions_co2_mt) emissions_co2_mt,
   COUNT(DISTINCT ssvid) n_distinct_ssvid
 FROM
-  `world-fishing-827.proj_ocean_ghg.monthly_spatial_vessel_emissions_by_receiver_type_{run_version_ais}`
-WHERE EXTRACT(YEAR FROM month) BETWEEN {analysis_start_year} AND {analysis_end_year}
-
+all_data
 GROUP BY
-  year
-)
+  year)
+ORDER BY
+year,
+receiver_type
