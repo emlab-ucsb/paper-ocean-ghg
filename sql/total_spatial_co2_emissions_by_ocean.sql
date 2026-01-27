27b96386224215bdff32656c5acc26cdc0aa3a79
gmcdonald-sfg <gmcdonald@bren.ucsb.edu>
2026-01-26 16:39:40 -0800

    update to next model version

--- sql/total_spatial_co2_emissions_by_ocean.sql
+++ sql/total_spatial_co2_emissions_by_ocean.sql
@@ -1,7 +1,10 @@
+WITH
+ocean_emissions_info AS(
 SELECT
    EXTRACT(YEAR FROM time) year,
    ocean,
-    sum(emissions_co2_mt + emissions_co2_dark_mt) emissions_co2_mt
+    SUM(emissions_co2_mt) emissions_co2_mt,
+    SUM(emissions_co2_dark_mt) emissions_co2_dark_mt
 FROM
   `world-fishing-827.proj_ocean_ghg.rf_s1_time_gridded_dark_fleet_model_{run_version_dark}`
   JOIN
@@ -13,4 +16,24 @@ WHERE
   EXTRACT(YEAR FROM time) BETWEEN {analysis_start_year} AND {analysis_end_year}
 GROUP BY
     year,
-    ocean
+    ocean),
+ocean_fraction_imaged_info AS(
+  SELECT
+ocean,
+EXTRACT(YEAR FROM time) year,
+COUNT(DISTINCT CONCAT(time,lon_bin,lat_bin)) number_observations,
+COUNT(DISTINCT IF(pixel_area_imaged_m2>0,CONCAT(time,lon_bin,lat_bin),NULL)) number_observations_imaged
+FROM `world-fishing-827.proj_ocean_ghg.rf_model_features_{run_version_dark}`
+WHERE
+  EXTRACT(YEAR FROM time) BETWEEN {analysis_start_year} AND {analysis_end_year}
+GROUP BY
+ocean,
+year
+)
+SELECT
+*
+FROM
+ocean_emissions_info
+JOIN
+ocean_fraction_imaged_info
+USING(ocean,year)
